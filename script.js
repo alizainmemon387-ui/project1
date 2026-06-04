847048d066e8bd1fe6deb5cdf611fc2fae3908fe
Ali Zain <alizainmemon@gmail.com>
2026-06-04 17:10:50 +0500

    Pushed files

---
+++ script.js
@@ -0,0 +1,115 @@
+// / var num1=24;
+// var num2=29;
+
+
+
+// console.log(num1*num2);
+// console.log(num2/num1);
+
+
+
+
+// let num1= Number(prompt("enter number"))
+// let num2= Number(prompt("enter number"))
+// alert(num1/num2)
+
+
+
+// let input=prompt("Enter your name: ");
+
+// if(input=="Zain"){
+//     console.log("Hello zain")
+// }
+
+
+// else if(input=="123"){
+//     console.log("numbers")
+// }
+
+
+// else{
+//     console.log("Hello unknown")
+// }
+
+
+// let num=prompt("enter a number")
+
+// if(num>90){
+//     console.log("greater than 90")
+// }
+
+
+// else if(num>100){
+//     console.log("greater than 100")
+// }
+
+// else{
+//     console.log("none")
+// }
+
+
+
+// let input=Number(prompt("enter your math marks:"))
+// let input2=Number(prompt(" enter your english marks:"))
+
+
+// if (input>=80 && input2<=80){
+//     console.log("you got A+ grade in math and english")
+// }
+
+
+
+// else if (input>=50 && input2<=50){
+//     console.log("you got B+ grade in math and english")
+// }
+
+// else{
+//     console.log("wrong input")
+// }
+
+
+
+// const h1= document.getElementById("h1")
+
+// const navAnchors = document.querySelectorAll("nav a");
+
+// navAnchors.forEach(anchor => {
+//   anchor.style.Color = "aqua";
+// });
+
+
+
+const btn= document.getElementById("btn")
+btn.addEventListener("dblclick", function(){
+    btn.classList.toggle("btttn");
+})
+
+
+// const heading1 = document.querySelector('#heading1');
+
+// if (heading1)  heading1.textContent = "This is now changed";
+  
+//   heading1.addEventListener("click", function () {
+//     this.classList.toggle("makecolorred");
+//   });
+// }
+
+
+// document.addEventListener("DOMContentLoaded", () => {
+//   const btn = document.getElementById("btn");
+//   btn.addEventListener("dblclick", function () {
+//     btn.classList.toggle("btttn");
+//   });
+// });
+
+
+
+// console.log("HHH")
+
+
+
+// $(funcvtion(){
+//     $("#btn").click(function(){
+//         $("#box") .toggleclass("hide");
+//     });
+// });
